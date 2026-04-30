6270cc728e0555003521fd361e377a8382458de7
einnivd <yelk0427@gmail.com>
2026-04-30 18:11:28 +0900

    할인배너 모달창 오늘 하루 보지않기 / 예정: 제품상세 페이지

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,23 @@
 window.addEventListener("load", function () {
-  // 모달창 닫기
+// 모달창 닫기
   const modalWrap = this.document.querySelector(".modal-wrap");
   const modalClose = this.document.querySelector("#close");
   modalClose.addEventListener("click", function () {
     modalWrap.style.display = "none";
   });
+});
+
+// 오늘 하루 보지않기
+$(function(){
+    let chk = $("#check")
+    // console.log(check);
+    if($.cookie("popup") !== "none"){
+        $(".modal-box").fadeIn("fast");
+    }  
+    $(".close").click(function(){
+        if(chk.is(":checked")){
+            $.cookie("popup" , "none" , {expires:1 , path:"/"});
+        }
+        $(".modal-box").fadeOut("fast");
+    });
 });
